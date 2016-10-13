27a5f4622497236db6fc797b663164cfaf3044ec
Jukka Puranen <jukka.puranen@gmail.com>
2016-10-13 08:55:10 +0300

    trying to figure out how to do the pixel map

--- lib/image_utilities.ex
+++ lib/image_utilities.ex
@@ -2,17 +2,28 @@ defmodule ImageUtilities do
   require Tensor
   def build_palette_map(pixels, width, heigth, background) do
 
-    pixelMatrix = Matrix.new(height,width)
+    pixel_matrix = Matrix.new(heigth,width)
+
+    palette_map = pixels
+    |> Stream.with_index
+    |> Stream.reduce(%{pixel_matrix: pixel_matrix, palette: %{} }, fn({pixel_row, y}, acc) ->
+      pixel_row
+      |> Stream.with_index
+      |> Stream.reduce(acc, fn({pixel, x}) ->
+        palette_index = map_size(acc.palette)
+        Map.put_new(acc.palette, pixel, palette_index)
+      end)
+    end)
 
     
 
 
-    palette_map = Enum.reduce(pixels,%{}, fn(pixel_row, %{palette_map) ->
-      Enum.reduce(pixel_row, palette_map, fn(pixel, palette_map) ->
-        # The size will be the index in the pixel array
-        Map.put_new(palette_map, pixel, map_size(palette_map))
-      end)
-    end)
+    # palette_map = Enum.reduce(pixels,%{}, fn(pixel_row, %{palette_map) ->
+    #   Enum.reduce(pixel_row, palette_map, fn(pixel, palette_map) ->
+    #     # The size will be the index in the pixel array
+    #     Map.put_new(palette_map, pixel, map_size(palette_map))
+    #   end)
+    # end)
 
     # If there is a background, it could be a color that never appears on the
     # image. If so, we have to ensure it is in the palette
